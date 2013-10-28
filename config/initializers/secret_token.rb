9cb9855338a43136e1134a8b36c30e5949f80c35
DanL <ddlinde@hotmail.com>
2013-10-28 09:15:16 +0000

    new rails app, createdb, rspec installed

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sifiapp::Application.config.secret_key_base = '4971ef8289213395477ddc35e223e6973d451cdf6efd97595f0ddedadd662867d9af63a09c871a68cf996efd360fa10d0ef548c8557d75254a0ed3f1f412aa9e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Sifiapp::Application.config.session_store :cookie_store, key: '_sifiapp_session'
