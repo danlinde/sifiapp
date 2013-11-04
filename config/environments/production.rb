5886815c505bbba5a5c7d3679b2f42d6cb80d54e
DanL <ddlinde@hotmail.com>
2013-11-04 11:45:55 +0000

    Add mailgun to Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,18 @@ Sifiapp::Application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { :host => 'sleepy-lake-4098.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+     :authentication => :plain,
+     :address => "smtp.mailgun.org",
+     :port => 587,
+     :domain => "sifiapplication.mailgun.org",
+     :user_name => "postmaster@sifiapplication.mailgun.org",
+     :password => "2tidda2-j314"
+  }
+  config.action_mailer.default_url_options = { :host => 'http://sleepy-lake-4098.herokuapp.com' }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
