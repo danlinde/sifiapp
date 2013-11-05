9223682ad2783c619c0d490e241706bbe8250e29
xiaoting wright <haley.xt.wright@gmail.com>
2013-11-05 19:32:44 +0000

    merge conflict

--- spec/controllers/charges_controller_spec.rb
+++ spec/controllers/charges_controller_spec.rb
@@ -1,15 +1,22 @@
 require 'spec_helper'
 
 describe ChargesController do
-	xit "updates participant status to paid" do
-		event = create(:event)
-		post :create, event_id: event, participant: attributes_for(:participant)
-		@participant.paid.should eq true
+	let(:participant){create(:participant)}
+	before do
+		session[:participant_token] = participant.token
 	end
 
-	xit "renders event page after charge made" do
+	it "updates participant status to paid" do
+		event = create(:event)
+		post :create, event_id: event, participant: participant
+
+		expect(Participant.find_by_token(participant.token)).to be_valid
+		expect(assigns[:participant].paid).to eq true
+	end
+
+	it "renders event page after charge made" do
 		event = create(:event)
 		post :create, event_id: event
-		expect(response).to redirect_to event
+		expect(response.status).to eq 200
 	end
 end
