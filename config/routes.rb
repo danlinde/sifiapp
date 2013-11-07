9117af40c9392184e942fbd15918a5cdf2924cca
kiy Hong Tang <kiyhong@gmail.com>
2013-11-07 09:21:36 +0000

    styled sign-in

--- config/routes.rb
+++ config/routes.rb
@@ -23,7 +23,7 @@ Sifiapp::Application.routes.draw do
   get '/authentications/get_email' => 'authentications#get_email'
   patch '/authentications/set_email' => 'authentications#set_email'
 
-
+  get '/about' => 'pages#about'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
